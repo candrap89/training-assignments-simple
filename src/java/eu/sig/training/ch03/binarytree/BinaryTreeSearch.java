e85891cce9dc4c73aec9460e5e161842fc1d9f08
Candra Setiyono <candra.setiyono@btpn.com>
2017-05-15 14:44:47 +0700

    binary search after commit

--- src/java/eu/sig/training/ch03/binarytree/BinaryTreeSearch.java
+++ src/java/eu/sig/training/ch03/binarytree/BinaryTreeSearch.java
@@ -8,23 +8,32 @@ public class BinaryTreeSearch {
         if (node.getValue() == nodeValue) {
             return depth;
         } else {
-            if (nodeValue < node.getValue()) {
-                BinaryTreeNode<Integer> left = node.getLeft();
-                if (left == null) {
-                    throw new TreeException("Value not found in tree!");
-                } else {
-                    return 1 + calculateDepth(left, nodeValue);
-                }
-            } else {
-                BinaryTreeNode<Integer> right = node.getRight();
-                if (right == null) {
-                    throw new TreeException("Value not found in tree!");
-                } else {
-                    return 1 + calculateDepth(right, nodeValue);
-                }
-            }
+//            if (nodeValue < node.getValue() && node.getLeft() != null) {
+//            	return 1+ calculateDepth(node.getLeft(), nodeValue);          	
+//            } else {
+//            	return 1+ calculateDepth(node.getRight(), nodeValue);
+//            }
+        	return traversalByValue(node, nodeValue);
         }
     }
     // end::calculateDepth[]
+    
+   private static int traversalByValue(BinaryTreeNode<Integer> node, int nodeValue){
+	   BinaryTreeNode<Integer> childNode = getChildNode(node, nodeValue);
+	   if (childNode == null) {
+		   throw new TreeException("Value not found in the tree");
+	   }else {
+		   return 1+ calculateDepth(childNode, nodeValue);
+	   }
+   }
 
+   
+   private static BinaryTreeNode<Integer> getChildNode(BinaryTreeNode<Integer> node, int nodeValue){
+	   if (nodeValue < node.getValue()){
+		   return node.getLeft();
+		   
+	   }else {
+		   return node.getRight();
+	   }
+   }
 }
