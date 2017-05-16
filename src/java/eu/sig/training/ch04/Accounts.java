8b810a1de8813fd754933fa4b7134cc36979bf02
Candra Setiyono <candra.setiyono@btpn.com>
2017-05-16 11:42:34 +0700

    fix inherit account

--- src/java/eu/sig/training/ch04/Accounts.java
+++ src/java/eu/sig/training/ch04/Accounts.java
@@ -6,6 +6,14 @@ public  class Accounts {
         return new CheckingAccount();
     }
 
+    public static boolean isValid(String number){
+    	int sum = 0;
+    	for (int i = 0; i< number.length();i++){
+    		sum = sum +(9-i)* Character.getNumericValue(number.charAt(i));
+    	}
+    	return sum % 11 == 0;
+    }
+    
     public Transfer makeTransfer (String counterAccount, Money amount) throws BusinessException {
     
     int sum = 0;
